880d6bd1fd61c61dd7f91ad5b25579a612113519
LeonardoCallauR <leo_cr_m@hotmail.com>
2020-02-15 12:08:11 -0400

    Mi primer modelo

---
+++ WebApplication1/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Person
+    {
+        [Key]
+        public int PersonID { get; set; }
+        [Required(ErrorMessage ="Dime tu nombre putita")]
+        [Display(Name ="Tu puto nombre completo")]
+        [StringLength(100,ErrorMessage ="Mas corto perra maximo 100")]
+        public string Name { get; set; }
+        [Display(Name ="CI pinche cabron")]
+        public string Document { get; set; }
+    }
+}
